ca09b60e6f660b9bf286aaa909ba18bc74837292
PulatovSO <pulatovsirotillo@gmail.com>
2023-07-11 08:03:23 +0500

    feat: add controls & animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 // sizes
 const width = window.innerWidth;
@@ -13,10 +14,14 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
 // axes
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 camera.position.set(0, 2, 5)
+controls.update();
 
 // box geometry
 const boxGeometry = new THREE.BoxGeometry();
@@ -24,4 +29,14 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-renderer.render( scene, camera );
+// animation
+const animate = () => {
+    requestAnimationFrame( animate );
+
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+
+animate();
